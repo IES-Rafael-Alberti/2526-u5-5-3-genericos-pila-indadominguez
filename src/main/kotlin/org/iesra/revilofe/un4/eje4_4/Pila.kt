e9ee5d5fdec7b0a6f75daf00831ab92510e1a27b
dhindalecio <dhindalecio@iesrafaelalberti.es>
2026-02-27 00:33:05 +0100

    Ejercicio resuelto

--- src/main/kotlin/org/iesra/revilofe/un4/eje4_4/Pila.kt
+++ src/main/kotlin/org/iesra/revilofe/un4/eje4_4/Pila.kt
@@ -22,8 +22,30 @@ class Pila<T> : Iterable<T> {
     val size: Int
         get() = pila.size
 
-    override fun toString(): String {
-        "Pila: $pila"
+    override fun toString(): String = "Pila: $pila"
+
+    override fun iterator(): Iterator<T> = object : Iterator<T> {
+        private var indice = pila.lastIndex
+        override fun hasNext(): Boolean = indice >= 0
+        override fun next() : T {
+            if (!hasNext()) throw NoSuchElementException()
+            return pila[indice--]
+
+        }
     }
 }
 
+fun <T> reverse(lista: List<T>): List<T> {
+    val pila = Pila<T>()
+    val iterador = lista.iterator()
+
+    while (iterador.hasNext()) {
+        pila.push(iterador.next())
+    }
+
+    val invertida = mutableListOf<T>()
+    while (!pila.isEmpty()) {
+        invertida.add(pila.pop())
+    }
+    return invertida
+}
